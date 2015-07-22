a2f3588af552dadbda1f910c5be1426a69d0471d
Byron Hallett <byron.james.h@gmail.com>
2015-07-23 00:29:54 +1000

    Added rmagick, added cheque image, cheque functions, cheque as rootpage

--- app/controllers/cheques_controller.rb
+++ app/controllers/cheques_controller.rb
@@ -1,17 +1,38 @@
+require 'RMagick'
+include Magick
+
 class ChequesController < ApplicationController
+	def index
+    	@cheques = Cheque.all
+  	end
+
+	def show		
+		# Create a 100x100 red image.
+		image = Image.new(100,100) { self.background_color = "red" }
+		image.display
+		exit
+		@cheque = Cheque.find(params[:id])
+	end
+
 	def new
 	end
 	
 	def create
 		@cheque = Cheque.new(cheque_params)
- 
-  	@cheque.save
-  	redirect_to @cheque
+ 		
+		@cheque.save
+		redirect_to @cheque
+	end
+
+	def destroy
+		@cheque = Cheque.find(params[:id])
+		@cheque.destroy
+
+		redirect_to cheques_path
 	end
 
 	private
 	  def cheque_params
 	    params.require(:cheque) .permit(:name, :date, :nominal_value)
 	  end
-
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,5 @@
 class LoginController < ApplicationController
   def index
+  	redirect_to cheques_path
   end
 end
