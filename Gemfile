7ad03ae381d0b54873ad0971f3e6a1b7ebe6a2d4
Byron Hallett <byron.james.h@gmail.com>
2015-07-23 04:42:51 +1000

    Version one complete
    Added cheque generation with humanised text.
    Added filtering system based on name (needed to force array type on single result pages to allow for each method to work)

--- Gemfile
+++ Gemfile
@@ -26,6 +26,12 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 #Bundle rmagick for image creation
 gem 'rmagick'
 
+# Bunle Humanize for turning number into readable words
+gem 'humanize'
+
+# Bundle filterific for table filters
+# gem 'filterrific'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/cheques_controller.rb
+++ app/controllers/cheques_controller.rb
@@ -2,16 +2,61 @@ require 'RMagick'
 include Magick
 
 class ChequesController < ApplicationController
-	def index
-    	@cheques = Cheque.all
-  	end
+	
+	def drawAndSaveCheque (name, date, value)
+		@chequeImage = ImageList.new('app/assets/images/cheque.png')
+		# Add the name text to the cheque in position
+		name_text = Draw.new
+		name_text.annotate(@chequeImage, 0,0,210,215, name) do
+		  self.gravity = NorthWestGravity
+		  self.pointsize = 30
+		  self.font_family = "Arial"
+		end
+		# Calculate a usable date string
+		dateString = date.strftime("%d / %m / %Y")
+		# Add the date text to the cheque in position
+		date_text = Draw.new
+		date_text.annotate(@chequeImage, 0,0,850,130, dateString) do
+		  self.gravity = NorthWestGravity
+		  self.pointsize = 30
+		  self.font_family = "Arial"
+		end
+		# Calculate both numeric and worded strings for value
+		valueNumString = value.to_s
+		# Now, an interesting part, converting number to string as words:
+		wordDollars = (value - value.modulo(1)).round.humanize
+		wordCents = ((value.modulo(1) * 100).round).humanize
+		valueWordString = wordDollars.to_s + " dollars and " + wordCents.to_s + " cents"
+		valueWordString = valueWordString.titleize
 
-	def show		
-		# Create a 100x100 red image.
-		image = Image.new(100,100) { self.background_color = "red" }
-		image.display
-		exit
+		# Add the nominal value text to the cheque in position
+		num_text = Draw.new
+		num_text.annotate(@chequeImage, 0,0,1020,220, valueNumString) do
+		  self.gravity = NorthWestGravity
+		  self.pointsize = 30
+		  self.font_family = "Arial"
+		  self.font_weight = BoldWeight
+		end
+		word_text = Draw.new
+		word_text.annotate(@chequeImage, 600,100,80,280, valueWordString) do
+		  self.gravity = NorthWestGravity
+		  self.pointsize = 30
+		  self.font_family = "Arial"
+		end
+		@chequeImage.write('public/images')
+	end
+
+	def index
+		if params[:name] != nil
+			@cheques = Array(Cheque.find_by(name: params[:name]))
+  	else
+  		@cheques = Cheque.all
+  	end
+	end
+
+	def show
 		@cheque = Cheque.find(params[:id])
+		drawAndSaveCheque(@cheque.name, @cheque.date, @cheque.nominal_value)
 	end
 
 	def new
